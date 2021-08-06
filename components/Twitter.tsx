cb957e7fcdc29840e82a66968e86ba821638fce8
yasufumimyoi <yasufumi.myoi@gmail.com>
2021-08-06 14:35:58 +0900

    Twitterの追加

---
+++ components/Twitter.tsx
@@ -0,0 +1,8 @@
+import { Share } from 'react-twitter-widgets'
+
+export const Large = () => (
+  <Share
+    url="https://video-learning-a4c3a.web.app/"
+    options={{ hashtags: '今日の積み上げ', size: 'large' }}
+  />
+)
